65212b89c0b776daef8c79510218e92f503ab1a2
Anudeep Roy <48355379+AnudeepRoy@users.noreply.github.com>
2019-06-10 17:29:33 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -7,25 +7,38 @@ Vue.component('num',{
 
 Vue.component('opr',{
 	template:`
-		<button type='button' class='btn btn-dark btn-block' ><slot></slot></button>
+		<button type='button' class='btn btn-dark btn-block' @click="$emit('add')"><slot></slot></button>
 	`
-})
+});
+
+Vue.component('calci',{
+	template:`
+		<button type='button' class='btn btn-success btn-block' @click="$emit('add')"><slot></slot></button>
+	`
+});
 
 var app = new Vue({
 	el:'#root',
 	data:{
-		result:[],
-		
-		
+		result:'',
 	},
+
 	methods:{
 		addNum(num){
-			this.result.push(num);
-			
+			this.result = this.result + num;
 		},
 
 		clearAll(){
 			this.result = [];
+		},
+
+		addSym(sym){
+			if(this.result.length != 0 || sym == '.'){
+			this.result = this.result + sym;
+			}
+		},
+		calculate(){
+			this.result = eval(String(this.result));
 		}
 	}
 	
